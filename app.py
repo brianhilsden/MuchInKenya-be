98209178be730bcf5c16bbbe5de032076fc00b5f
victor kamau <vikakamau04@gmail.com>
2024-07-09 22:47:15 +0300

    initial

--- app.py
+++ app.py
@@ -1,1 +1,100 @@
-f
+from flask import Flask,request,make_response
+from flask_migrate import Migrate 
+from flask_restful import Api, Resource 
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://muchinkenya.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.json.compact = False
+
+migrate = Migrate(app,db)
+db.init_app(app)
+
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return {"message":"Welcome to MuchInKenya"}
+
+app.add_resource(Home,'/')
+
+class Foods(Resource):
+    def get (self):
+        pass
+
+    def post (self):
+        pass
+
+app.add_resource(Foods,'/foods')
+
+class Restaurant(Resource):
+    def get (self):
+        pass
+
+    def post(self):
+        pass
+
+app.add_resource(Restaurant,'/restaurant')
+
+class Drivers(Resource):
+    def get (self):
+        pass
+
+    def post(self):
+        pass
+
+app.add_resource(Drivers, '/drivers')
+
+class Restaurant_food(Resource):
+    def get (self):
+        pass
+
+app.add_resource(Restaurant_food, '/restaurant_food')
+
+
+class Orders(Resource):
+    def get (self):
+        pass
+
+    def post(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+app.add_resource(Orders, '/orders')
+
+class Customer(Resource):
+    def get (self):
+        pass
+
+    def post(self):
+        pass
+
+    def patch(self):
+        pass
+
+app.add_resource(Customer, '/customers')
+
+class Reviews(Resource):
+    def get (self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+app.add_resource(Reviews, '/reviews')
+
+
+
+
+if __name__ == '__main___':
+    app.run(port=5555,debug=True)
