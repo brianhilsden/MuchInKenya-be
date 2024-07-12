1c1909dd869f75fa2760a60b89b6d27e27c1e65e
Brian <brianhilsden@gmail.com>
2024-07-13 00:20:33 +0300

    added postress db

--- migrations/versions/4cd3e4286527_initial_migration.py
+++ migrations/versions/4cd3e4286527_initial_migration.py
@@ -1,8 +1,8 @@
 """initial migration
 
-Revision ID: 0c5f39dc6674
+Revision ID: 4cd3e4286527
 Revises: 
-Create Date: 2024-07-10 16:33:10.276051
+Create Date: 2024-07-13 00:14:04.808287
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0c5f39dc6674'
+revision = '4cd3e4286527'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,6 +23,7 @@ def upgrade():
     sa.Column('name', sa.String(), nullable=True),
     sa.Column('email', sa.String(), nullable=True),
     sa.Column('phone_number', sa.String(), nullable=True),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('drivers',
@@ -36,12 +37,14 @@ def upgrade():
     sa.Column('name', sa.String(), nullable=True),
     sa.Column('description', sa.String(), nullable=True),
     sa.Column('price', sa.Integer(), nullable=True),
+    sa.Column('image', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('restaurants',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(), nullable=True),
     sa.Column('location', sa.String(), nullable=True),
+    sa.Column('image', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('orders',
