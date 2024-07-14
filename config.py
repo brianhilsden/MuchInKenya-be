21a6ad5e3cf253300364ea0df23eae7b5972d016
Brian <brianhilsden@gmail.com>
2024-07-14 11:30:26 +0300

    linked postgress

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///munchinkenya.db"
-""" app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') """
+""" app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///munchinkenya.db" """
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 db = SQLAlchemy()
@@ -21,7 +21,6 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
-
 
 api = Api(app)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
